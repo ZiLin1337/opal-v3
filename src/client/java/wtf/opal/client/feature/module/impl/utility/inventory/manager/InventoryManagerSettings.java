5fdea235bc4c982a3336f422196aa78363b64bf2
ZiLin1337 <zhangmm1102@hotmail.com>
2026-01-21 18:32:00 +0800

    update inv

--- src/client/java/wtf/opal/client/feature/module/impl/utility/inventory/manager/InventoryManagerSettings.java
+++ src/client/java/wtf/opal/client/feature/module/impl/utility/inventory/manager/InventoryManagerSettings.java
@@ -11,9 +11,8 @@ public final class InventoryManagerSettings {
     private final BoundedNumberProperty delay;
 
     private final MultipleBooleanProperty slots;
-    private final NumberProperty swordSlot, pickaxeSlot, axeSlot, blockSlot /*bowSlot, potionSlot, goldenAppleSlot, enderPearlSlot*/;
+    private final NumberProperty swordSlot, pickaxeSlot, axeSlot, blockSlot, bowSlot, potionSlot, goldenAppleSlot, enderPearlSlot;
 
-    // TODO: implement the commented out stuff
     public InventoryManagerSettings(final InventoryManagerModule module) {
         this.delay = new BoundedNumberProperty("Delay", 50, 100, 0, 400, 5);
 
@@ -21,23 +20,23 @@ public final class InventoryManagerSettings {
                 new BooleanProperty("Sword", true),
                 new BooleanProperty("Pickaxe", true),
                 new BooleanProperty("Axe", true),
-                new BooleanProperty("Blocks", true)
-//                new BooleanProperty("Bow", true),
-//                new BooleanProperty("Potions", true),
-//                new BooleanProperty("Golden Apples", true),
-//                new BooleanProperty("Ender Pearls", true)
+                new BooleanProperty("Blocks", true),
+                new BooleanProperty("Bow", true),
+                new BooleanProperty("Potions", true),
+                new BooleanProperty("Golden Apples", true),
+                new BooleanProperty("Ender Pearls", true)
         );
 
         this.swordSlot = new NumberProperty("Sword Slot", 1, 1, 9, 1).hideIf(() -> !slots.getProperty("Sword").getValue());
         this.pickaxeSlot = new NumberProperty("Pickaxe Slot", 2, 1, 9, 1).hideIf(() -> !slots.getProperty("Pickaxe").getValue());
         this.axeSlot = new NumberProperty("Axe Slot", 3, 1, 9, 1).hideIf(() -> !slots.getProperty("Axe").getValue());
         this.blockSlot = new NumberProperty("Block Slot", 4, 1, 9, 1).hideIf(() -> !slots.getProperty("Blocks").getValue());
-//        this.bowSlot = new NumberProperty("Bow Slot", 5, 1, 9, 1).hideIf(() -> !slots.getProperty("Bow").getValue());
-//        this.potionSlot = new NumberProperty("Potion Slot", 6, 1, 9, 1).hideIf(() -> !slots.getProperty("Potions").getValue());
-//        this.goldenAppleSlot = new NumberProperty("Golden Apple Slot", 7, 1, 9, 1).hideIf(() -> !slots.getProperty("Golden Apples").getValue());
-//        this.enderPearlSlot = new NumberProperty("Ender Pearl Slot", 8, 1, 9, 1).hideIf(() -> !slots.getProperty("Ender Pearls").getValue());
+        this.bowSlot = new NumberProperty("Bow Slot", 5, 1, 9, 1).hideIf(() -> !slots.getProperty("Bow").getValue());
+        this.potionSlot = new NumberProperty("Potion Slot", 6, 1, 9, 1).hideIf(() -> !slots.getProperty("Potions").getValue());
+        this.goldenAppleSlot = new NumberProperty("Golden Apple Slot", 7, 1, 9, 1).hideIf(() -> !slots.getProperty("Golden Apples").getValue());
+        this.enderPearlSlot = new NumberProperty("Ender Pearl Slot", 8, 1, 9, 1).hideIf(() -> !slots.getProperty("Ender Pearls").getValue());
 
-        module.addProperties(delay, new GroupProperty("Slots", slots, swordSlot, pickaxeSlot, axeSlot, blockSlot /*bowSlot, potionSlot, goldenAppleSlot, enderPearlSlot*/));
+        module.addProperties(delay, new GroupProperty("Slots", slots, swordSlot, pickaxeSlot, axeSlot, blockSlot, bowSlot, potionSlot, goldenAppleSlot, enderPearlSlot));
     }
 
     public Double getDelay() {
@@ -64,19 +63,19 @@ public final class InventoryManagerSettings {
         return blockSlot.getValue().intValue();
     }
 
-//    public int getBowSlot() {
-//        return bowSlot.getValue().intValue();
-//    }
-//
-//    public int getPotionSlot() {
-//        return potionSlot.getValue().intValue();
-//    }
-//
-//    public int getGoldenAppleSlot() {
-//        return goldenAppleSlot.getValue().intValue();
-//    }
-//
-//    public int getEnderPearlSlot() {
-//        return enderPearlSlot.getValue().intValue();
-//    }
+    public int getBowSlot() {
+        return bowSlot.getValue().intValue();
+    }
+
+    public int getPotionSlot() {
+        return potionSlot.getValue().intValue();
+    }
+
+    public int getGoldenAppleSlot() {
+        return goldenAppleSlot.getValue().intValue();
+    }
+
+    public int getEnderPearlSlot() {
+        return enderPearlSlot.getValue().intValue();
+    }
 }
